59bac3ca0b3af59e23e454781d624f67044f55e0
Amanda <amanda.profissional078@gmail.com>
2025-02-28 21:33:47 -0300

    Model

---
+++ src/main/java/model/Contato.java
@@ -0,0 +1,24 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity(name = "tb_contato")
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+}
