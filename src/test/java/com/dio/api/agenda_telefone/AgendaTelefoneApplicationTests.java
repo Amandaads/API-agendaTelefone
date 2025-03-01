a2351aed3a487084ed1a6bd2f9d788c2947e3d09
Amanda <amanda.profissional078@gmail.com>
2025-02-28 21:34:52 -0300

    Tests

---
+++ src/test/java/com/dio/api/agenda_telefone/AgendaTelefoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.api.agenda_telefone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTelefoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
